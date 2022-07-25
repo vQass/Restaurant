1915c0a5b2689c2ce69c8f1a57b76bbd4473d6df
Patryk PC <Patryk.Zub@interia.pl>
2022-07-25 20:44:01 +0200

    CityController

---
+++ Restaurant.API/Controllers/CityController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        public CityController()
+        {
+
+        }
+
+        [HttpGet("GetCityList")]
+        public IActionResult GetCityList()
+        {
+
+            return Ok();
+        }
+
+        [HttpGet("GetCityById/{id}")]
+        public IActionResult GetCityById([FromRoute] short id)
+        {
+
+            return Ok();
+        }
+
+        [HttpPost("AddCity")]
+        public IActionResult AddCity([FromBody] string cityName)
+        {
+
+            return Ok();
+        }
+
+        [HttpPut("UpdateCity/{id}")]
+        public IActionResult UpdateCity([FromRoute] short id, [FromBody] string cityName)
+        {
+
+            return Ok();
+        }
+
+        [HttpDelete("DeleteCity/{id}")]
+        public IActionResult DeleteCity([FromRoute] short id)
+        {
+
+            return Ok();
+        }
+
+        [HttpPut("EnableCity/{id}")]
+        public IActionResult EnableCity([FromRoute] short id)
+        {
+
+            return Ok();
+        }
+
+        [HttpPut("DisableCity/{id}")]
+        public IActionResult DisableCity([FromRoute] short id)
+        {
+
+            return Ok();
+        }
+    }
+}
