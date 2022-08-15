8cb489dd93badb99dda833525bf1b4280474a472
Patryk PC <Patryk.Zub@interia.pl>
2022-08-15 18:43:42 +0200

    Diagnostics helpers

---
+++ DiagnosticsHelpers/StopwatchHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace DiagnosticsHelpers
+{
+    public static class StopwatchHelper
+    {
+        private static List<string> _measurementsResults;
+        private static Dictionary<string, Stopwatch> _stopwatches;
+        static StopwatchHelper()
+        {
+            _measurementsResults = new List<string>();
+            _stopwatches = new Dictionary<string, Stopwatch>();
+        }
+
+        public static void Start(string name = "default")
+        {
+            try
+            {
+                _stopwatches.Add(name, new Stopwatch());
+                _stopwatches[name].Start();
+            }
+            catch
+            {
+
+            }
+        }
+
+        public static void Stop(string name = "default")
+        {
+            _stopwatches[name].Stop();
+            var elapsedTime = _stopwatches[name].ElapsedMilliseconds;
+            var resultMessage = $"Stopwatch {name} stopped after {elapsedTime} milliseconds";
+            _measurementsResults.Add(resultMessage);
+            _stopwatches.Remove(name);
+        }
+
+        public static void LogMessages()
+        {
+            _measurementsResults.ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+        }
+
+        public static void ClearMessages()
+        {
+            _measurementsResults.Clear();
+        }
+
+    }
+}
