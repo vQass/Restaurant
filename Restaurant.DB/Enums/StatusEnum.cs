1ea7564b0bf513e1f3d5cc02b4956e6e9310cbcb
Patryk PC <Patryk.Zub@interia.pl>
2022-08-17 19:27:39 +0200

    OrderStatuses changed to english, added dictionary to OrderStatuses

--- Restaurant.DB/Enums/StatusEnum.cs
+++ Restaurant.DB/Enums/StatusEnum.cs
@@ -2,9 +2,27 @@
 {
     public enum OrderStatusEnum : byte
     {
-        Realizowane,
-        Gotowe,
-        Anulowane,
-        Zawieszone
+        Pending,
+        InProgress,
+        Completed,
+        Cancelled,
+        Suspended
+    }
+
+    public static class OrderStatusDictionary
+    {
+        static OrderStatusDictionary()
+        {
+            OrderStatusesWithDescription = new Dictionary<OrderStatusEnum, string>()
+            {
+                { OrderStatusEnum.Pending, "W oczekiwaniu" },
+                { OrderStatusEnum.InProgress, "W trakcie realizajci"},
+                { OrderStatusEnum.Completed, "Zrealizowane"},
+                { OrderStatusEnum.Cancelled, "Anulowane"},
+                { OrderStatusEnum.Suspended, "Tymczasowo zawieszone"}
+            };
+        }
+
+        public static Dictionary<OrderStatusEnum, string> OrderStatusesWithDescription { get; }
     }
 }
