f23854e883c74b61a2dca89772b20c5eb67538a1
Patryk PC <Patryk.Zub@interia.pl>
2022-10-09 21:56:46 +0200

    cart button for mobile view

--- Restaurant.App/src/app/components/mainPage/order/order-page/order-page.component.ts
+++ Restaurant.App/src/app/components/mainPage/order/order-page/order-page.component.ts
@@ -1,16 +1,24 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { map, Observable, shareReplay } from 'rxjs';
 import { MealService } from 'src/app/services/ApiServices/meal.service';
 import { MealGroupViewModel } from 'src/models/meal/MealGroupViewModel';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+  styleUrls: ['./order-page.component.scss']
 })
 export class OrderPageComponent implements OnInit {
   mealGroups!: MealGroupViewModel[];
 
-  constructor(private mealService: MealService) { }
+  isMobileView$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private mealService: MealService, private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
     this.mealService.getGroupedMeals().subscribe(data => this.mealGroups = data);
