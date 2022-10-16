89e32a7f0f6933487ab6ebc34f0949db89ac7c02
Patryk PC <Patryk.Zub@interia.pl>
2022-10-16 20:01:11 +0200

    Added paging extension

---
+++ Restaurant.LinqHelpers/Helpers/PagingExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Restaurant.LinqHelpers.Helpers
+{
+    public static class PagingExtension
+    {
+        public static IQueryable<T> ApplyPaging<T>(this IQueryable<T> entities, int pageIndex = 0, int pageSize = 0)
+        {
+            if (!entities.Any())
+            {
+                return entities;
+            }
+
+            if(pageSize == 0)
+            {
+                return entities;
+            }
+
+            return entities
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize);
+        }
+    }
+}
