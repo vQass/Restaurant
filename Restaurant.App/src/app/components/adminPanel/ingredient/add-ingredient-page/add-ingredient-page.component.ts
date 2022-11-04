30c778092f5f4329219c4d0e37db269b9834324a
Patryk PC <Patryk.Zub@interia.pl>
2022-11-04 12:01:45 +0100

    Added components for adding and editing ingredients

---
+++ Restaurant.App/src/app/components/adminPanel/ingredient/add-ingredient-page/add-ingredient-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-ingredient-page',
+  templateUrl: './add-ingredient-page.component.html',
+  styleUrls: ['./add-ingredient-page.component.scss']
+})
+export class AddIngredientPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Restaurant.App/src/app/components/adminPanel/ingredient/edit-ingredient-page/edit-ingredient-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-ingredient-page',
+  templateUrl: './edit-ingredient-page.component.html',
+  styleUrls: ['./edit-ingredient-page.component.scss']
+})
+export class EditIngredientPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
